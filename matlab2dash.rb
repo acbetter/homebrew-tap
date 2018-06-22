8e8b961fa7af6a5ad53498fa784bec322dedb163
Likey <github@acbetter.com>
2018-06-22 12:37:26 +0800

    Update matlab2dash.rb

--- matlab2dash.rb
+++ matlab2dash.rb
@@ -1,7 +1,7 @@
 class Matlab2dash < Formula
   desc "Convert MATLAB Help Document to Docset for Dash.app to use"
-  homepage "https://github.com/acbetter/matlab-to-dash/releases"
-  url "https://github.com/acbetter/matlab-to-dash/releases/download/v1.2/matlab2dash.tgz"
+  homepage "https://github.com/acbetter/matlab-to-dash-docset/releases"
+  url "https://github.com/acbetter/matlab-to-dash-docset/releases/download/v1.2/matlab2dash.tgz"
   sha256 "4e56d4afd24642cd792c230bb0ea18e62a1791ab8e80d0185a3fb39abdc12206"
 
   def install
